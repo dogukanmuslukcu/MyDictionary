5a790592df8d5e48fb719d3724b79f3989cdb6e3
Dogukan Muslukcu <202003011008@dogus.edu.tr>
2021-01-21 21:05:38 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TW, TM>
+    {
+        TW[] words;
+        TM[] means;
+        public MyDictionary()
+        {
+            words = new TW[0];
+            means = new TM[0];
+        }
+
+        public void AddWords(TW word,TM mean )
+        {
+            TW[] wordsTemp = words;
+            TM[] meansTemp = means;
+
+            words = new TW[words.Length + 1];
+            means = new TM[means.Length + 1];
+            for (int i = 0; i < wordsTemp.Length; i++)
+            {
+                words[i] = wordsTemp[i];
+                means[i] = meansTemp[i];
+
+            }
+
+            words[words.Length - 1] = word; 
+
+        }
+        public TW[] Words
+        {
+            get { return words; }
+        }
+        public TM[] Means
+        {
+            get { return means; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> dictionary = new MyDictionary<string, string>();
+            dictionary.AddWords("Scholl", "Okul");
+            dictionary.AddWords("Unforgettable", "Unutulmaz");
+
+            foreach (var item in dictionary.Words)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
